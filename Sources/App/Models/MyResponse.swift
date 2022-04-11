193cb181f482d8d78a77fd97058bab98c3e2eaf2
yigua <xingyigua@gmail.com>
2022-04-11 20:58:05 +0800

    响应 model

---
+++ Sources/App/Models/MyResponse.swift
@@ -0,0 +1,18 @@
+//
+//  MyResponse.swift
+//  
+//
+//  Created by yigua on 2022/4/11.
+//
+
+import Vapor
+
+struct MyResponse<T: Content>: Content {
+    
+    var code = 20000
+    
+    var message = "ok"
+
+    let data: T
+    
+}
